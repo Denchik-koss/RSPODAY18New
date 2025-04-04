5569a7548f4744b8e4aa7a29bf036ebd39ecef73
Denchik-koss <kustikov18@gmail.com>
2025-04-04 09:54:09 +0300

    add commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace Day18RS
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Hotel> CurrentHotels { get; set; }
         public MainWindow()
         {
             InitializeComponent();
